d1e64147826f5bece3c8fc4c9c2e895e2711e41c
Alex Davidson <alex.davidson@clicksco.com>
2017-02-06 15:44:59 +0000

    settings

--- js/settings.js
+++ js/settings.js
@@ -14,6 +14,7 @@ t.render(function(){
   .spread(function(savedAdd, savedVegetable){
     if(savedAdd && /[a-z]+/.test(savedAdd)){
       addSelector.value = savedAdd;
+      alert(addSelector.value)
     }
     if(savedVegetable && /[a-z]+/.test(savedVegetable)){
       vegetableSelector.value = savedVegetable;
@@ -29,9 +30,6 @@ document.getElementById('save').addEventListener('click', function(){
   return t.set('board', 'private', 'vegetable', vegetableSelector.value)
   .then(function(){
     return t.set('board', 'private', 'add', addSelector.value);
-    if (addSelector.value = "no"){
-      alert("add = no")
-    }
   })
   .then(function(){
     t.closePopup();
