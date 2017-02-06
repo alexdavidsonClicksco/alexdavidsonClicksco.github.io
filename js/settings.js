8c9b51e6c5f9a623582546ddac8545c6328f15e5
Alex Davidson <alex.davidson@clicksco.com>
2017-02-06 15:34:46 +0000

    testing save

--- js/settings.js
+++ js/settings.js
@@ -8,14 +8,12 @@ var vegetableSelector = document.getElementById('vegetable');
 
 t.render(function(){
   return Promise.all([
-    t.get('board', 'shared', 'add'),
+    t.get('board', 'private', 'add'),
     t.get('board', 'private', 'vegetable')
   ])
   .spread(function(savedAdd, savedVegetable){
     if(savedAdd && /[a-z]+/.test(savedAdd)){
       addSelector.value = savedAdd;
-      alert("hello world")
-      alert(addSelector)
     }
     if(savedVegetable && /[a-z]+/.test(savedVegetable)){
       vegetableSelector.value = savedVegetable;
@@ -30,7 +28,10 @@ t.render(function(){
 document.getElementById('save').addEventListener('click', function(){
   return t.set('board', 'private', 'vegetable', vegetableSelector.value)
   .then(function(){
-    return t.set('board', 'shared', 'add', addSelector.value);
+    return t.set('board', 'private', 'add', addSelector.value);
+    if (addSelector.value = "no"){
+      alert("add = no")
+    }
   })
   .then(function(){
     t.closePopup();
